e10007c26c8937faa0ec4968ca29b7a22032fba3
Alex Elancheliyan <117940315+alexcheliyan@users.noreply.github.com>
2024-05-15 10:04:07 +0530

    Update settings.py

--- Day3/templatedemo/templatedemo/settings.py
+++ Day3/templatedemo/templatedemo/settings.py
@@ -22,7 +22,7 @@ STATIC_DIR = os.path.join(BASE_DIR2,'static')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ih95@ueo2&n^^o=f$5s3*ez!1aktu9awp1v+yv=+mt17#jw&tq'
+SECRET_KEY = 'MY SECRET KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
