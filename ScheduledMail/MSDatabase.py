aef4316adb2ae2f8bf2dfab66c50198dd1084f1d
Alex Elancheliyan <117940315+Alex-Elancheliyan@users.noreply.github.com>
2024-05-23 13:19:59 +0530

    Update MSDatabase.py

--- ScheduledMail/MSDatabase.py
+++ ScheduledMail/MSDatabase.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import declarative_base
 
 
 #ENGINE CREATION
-DB_URL ='postgresql://postgres:1234@localhost/AlexDataBase'
+DB_URL ='YOUR DB URL'
 engine = create_engine(DB_URL)
 SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
 Base = declarative_base()
