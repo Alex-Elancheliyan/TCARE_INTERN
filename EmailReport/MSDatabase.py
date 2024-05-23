117362521a3af02e7d82f2366717f1d052621f1a
Alex Elancheliyan <117940315+Alex-Elancheliyan@users.noreply.github.com>
2024-05-23 10:46:55 +0530

    Update MSDatabase.py

--- EmailReport/MSDatabase.py
+++ EmailReport/MSDatabase.py
@@ -5,23 +5,11 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import sessionmaker,Session
 from sqlalchemy.orm import declarative_base
 
-
-
-
 #ENGINE CREATION
-DB_URL ='postgresql://postgres:1234@localhost/AlexDataBase'
+DB_URL ='postgresql://postgres:your db credentials here'
 engine = create_engine(DB_URL)
 SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
 Base = declarative_base()
 
-#Base.metadata.create_all(bind=engine) --> #We can use this code if we wanted to create table without using the alembic.
-
-# #TO GET A SESSION TO USE
-# def get_db():
-#     db = SessionLocal()
-#     try:
-#         yield db
-#     finally:
-#         db.close()
 
 
